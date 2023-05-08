26743346e11ba0da27a6f9a589de54ea609cfac8
Giovanny Occident <GioOccident@gmail.com>
2023-05-08 00:12:12 -0400

    my-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './components/Navbar'
+import Accordion from "./components/Accordion"
+import { useState } from 'react';
 function App() {
+  const [i, setChar] = useState(0)
+  const [pg, setPage] = useState(2)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <div className="accordion-container">
+        <Accordion min={i} max={i + 10} page={pg} />
+        <Accordion min={i + 10} max={i + 20} page={pg} />
+        <Accordion min={i + 20} max={i + 30} page={pg} />
+        <Accordion min={i + 30} max={i + 40} page={pg} />
+        <Accordion min={i + 40} max={i + 50} page={pg} />
+      </div>
     </div>
   );
 }
